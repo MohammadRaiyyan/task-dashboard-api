809785d8290842c51fe73c733f2f224e0c238811
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-02-28 00:09:31 +0530

    added user in manifest

--- src/controllers/manifestController.ts
+++ src/controllers/manifestController.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from "express";
 import { TaskPriority, TaskStatus } from "../models/Config";
+import { User } from "../models/User";
 import { sendResponse } from "../utils/apiResponse";
 
 export const getManifests = async (req: Request, res: Response) => {
 	try {
 		const userId = req.user?.id;
+		const user = await User.findById(userId).select("-password");
+		if (!user) {
+			sendResponse(res, 401, "User not found");
+			return;
+		}
 		const statuses = await TaskStatus.find({ userId }).select("-userId");
 		const priorities = await TaskPriority.find({ userId }).select("-userId");
 		sendResponse(res, 200, "", {
+			user,
 			statuses,
 			priorities,
 		});
